de8c9c608223132234ff0686124a20ad2883b992
yshimizu37 <yshimizu@netapp.com>
2021-06-15 20:28:44 +0900

    add compiler

---
+++ ai-training-run.py
@@ -0,0 +1,146 @@
+# Kubeflow Pipeline Definition: AI Training Run
+
+import kfp.dsl as dsl
+import kfp.onprem as onprem
+
+# Define Kubeflow Pipeline
+@dsl.pipeline(
+    # Define pipeline metadata
+    name="AI Training Run",
+    description="Template for executing an AI training run with built-in training dataset traceability and trained model versioning"
+)
+def ai_training_run(
+    # Define variables that the user can set in the pipelines UI; set default values
+    dataset_volume_pvc_existing: str = "dataset-vol",
+    trained_model_volume_pvc_existing: str = "kfp-model-vol",
+    execute_data_prep_step__yes_or_no: str = "yes",
+    data_prep_step_container_image: str = "python:3",
+    # data_prep_step_command: str = "<insert command here>",
+    data_prep_step_dataset_volume_mountpoint: str = "/mnt/dataset",
+    train_step_container_image: str = "nvcr.io/nvidia/tensorflow:21.03-tf1-py3",
+    # train_step_command: str = "<insert command here>",
+    train_step_dataset_volume_mountpoint: str = "/mnt/dataset",
+    train_step_model_volume_mountpoint: str = "/mnt/model",
+    train_step_epochs: int = 15,
+    train_step_batch_size: int = 128,
+    validation_step_container_image: str = "python:3",
+    # validation_step_command: str = "<insert command here>",
+    validation_step_dataset_volume_mountpoint: str = "/mnt/dataset",
+    validation_step_model_volume_mountpoint: str = "/mnt/model",
+) :
+    # Set GPU limits; Due to SDK limitations, this must be hardcoded
+    train_step_num_gpu = 0
+    validation_step_num_gpu = 0
+
+    # Pipeline Steps:
+
+    # Execute data prep step
+    with dsl.Condition(execute_data_prep_step__yes_or_no == "yes") :
+        data_prep = dsl.ContainerOp(
+            name="data-prep",
+            image=data_prep_step_container_image,
+            command=["sh", "-c"],
+            arguments=["\
+                python3 -m pip install pandas && \
+                git clone https://github.com/yshimizu37/image_classification.git && \
+                chmod -R 755 image_classification &&  \
+                python3 ./image_classification/data_prep.py --datadir " + str(data_prep_step_dataset_volume_mountpoint) + "/cats_and_dogs_filtered"],
+        )
+        # Mount dataset volume/pvc
+        data_prep.apply(
+            onprem.mount_pvc(dataset_volume_pvc_existing, 'dataset', data_prep_step_dataset_volume_mountpoint)
+        )
+
+    # Create a snapshot of the dataset volume/pvc for traceability
+    volume_snapshot_name = "dataset-{{workflow.uid}}"
+    dataset_snapshot = dsl.ContainerOp(
+        name="dataset-snapshot",
+        image="python:3",
+        command=["/bin/bash", "-c"],
+        arguments=["\
+            python3 -m pip install ipython kubernetes pandas tabulate && \
+            git clone https://github.com/NetApp/netapp-data-science-toolkit && \
+            mv /netapp-data-science-toolkit/Kubernetes/ntap_dsutil_k8s.py / && \
+            echo '" + volume_snapshot_name + "' > /volume_snapshot_name.txt && \
+            /ntap_dsutil_k8s.py create volume-snapshot --pvc-name=" + str(dataset_volume_pvc_existing) + " --snapshot-name=" + str(volume_snapshot_name) + " --namespace={{workflow.namespace}}"],
+        file_outputs={"volume_snapshot_name": "/volume_snapshot_name.txt"}
+    )
+    # State that snapshot should be created after the data prep job completes
+    dataset_snapshot.after(data_prep)
+
+    # Execute training step
+    train = dsl.ContainerOp(
+        name="train-model",
+        image=train_step_container_image,
+        command=["sh", "-c"],
+        arguments=["\
+            python3 -m pip install pandas && \
+            git clone https://github.com/yshimizu37/image_classification.git && \
+            chmod -R 755 image_classification &&  \
+            echo '" + str(train_step_model_volume_mountpoint) + "/history.csv" + "' > /model_history.txt && \
+            python3 ./image_classification/train.py -datadir " + str(data_prep_step_dataset_volume_mountpoint) + "/cats_and_dogs_filtered --modeldir " + str(train_step_model_volume_mountpoint) + " --batchsize " + str(train_step_batch_size) + " --epochs " + str(train_step_epochs)],
+        file_outputs={
+            # "model_path": "/model_path.txt",
+            # "model_weights_path": "/model_weights_path.txt",
+            "model_history_path": "/model_history_path.txt"
+            }
+    )
+    # Mount dataset volume/pvc
+    train.apply(
+        onprem.mount_pvc(dataset_volume_pvc_existing, 'datavol', train_step_dataset_volume_mountpoint)
+    )
+    # Mount model volume/pvc
+    train.apply(
+        onprem.mount_pvc(trained_model_volume_pvc_existing, 'modelvol', train_step_model_volume_mountpoint)
+    )
+    # Request that GPUs be allocated to training job pod
+    if train_step_num_gpu > 0 :
+        train.set_gpu_limit(train_step_num_gpu, 'nvidia')
+    # State that training job should be executed after dataset volume snapshot is taken
+    train.after(dataset_snapshot)
+
+    # Create a snapshot of the model volume/pvc for model versioning
+    volume_snapshot_name = "kfp-model-{{workflow.uid}}"
+    model_snapshot = dsl.ContainerOp(
+        name="model-snapshot",
+        image="python:3",
+        command=["/bin/bash", "-c"],
+        arguments=["\
+            python3 -m pip install ipython kubernetes pandas tabulate && \
+            git clone https://github.com/NetApp/netapp-data-science-toolkit && \
+            mv /netapp-data-science-toolkit/Kubernetes/ntap_dsutil_k8s.py / && \
+            echo '" + volume_snapshot_name + "' > /volume_snapshot_name.txt && \
+            /ntap_dsutil_k8s.py create volume-snapshot --pvc-name=" + str(trained_model_volume_pvc_existing) + " --snapshot-name=" + str(volume_snapshot_name) + " --namespace={{workflow.namespace}}"],
+        file_outputs={"volume_snapshot_name": "/volume_snapshot_name.txt"}
+    )
+    # State that snapshot should be created after the training job completes
+    model_snapshot.after(train)
+
+    # Execute inference validation job
+    inference_validation = dsl.ContainerOp(
+        name="validate-model",
+        image=validation_step_container_image,
+        command=["sh", "-c"],
+        arguments=["\
+            python3 -m pip install pandas && \
+            git clone https://github.com/yshimizu37/image_classification.git && \
+            chmod -R 755 image_classification &&  \
+            python3 ./image_classification/validation.py --modeldir " + str(train_step_model_volume_mountpoint) + " --epochs=" + str(train_step_epochs)],
+    )
+    # Mount dataset volume/pvc
+    inference_validation.apply(
+        onprem.mount_pvc(dataset_volume_pvc_existing, 'datavol', validation_step_dataset_volume_mountpoint)
+    )
+    # Mount model volume/pvc
+    inference_validation.apply(
+        onprem.mount_pvc(trained_model_volume_pvc_existing, 'modelvol', validation_step_model_volume_mountpoint)
+    )
+    # Request that GPUs be allocated to pod
+    if validation_step_num_gpu > 0 :
+        inference_validation.set_gpu_limit(validation_step_num_gpu, 'nvidia')
+    # State that inference validation job should be executed after model volume snapshot is taken
+    inference_validation.after(model_snapshot)
+
+if __name__ == "__main__" :
+    import kfp.compiler as compiler
+    compiler.Compiler().compile(ai_training_run, __file__ + ".yaml")
--- data_prep.py
+++ data_prep.py
@@ -1,13 +1,4 @@
-import tensorflow as tf
-
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
 import os
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
 import argparse
 
 
